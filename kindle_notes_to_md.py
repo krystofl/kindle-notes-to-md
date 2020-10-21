bc53e9883783fffd5f1917e4b6f1c8348e720453
Krystof Litomisky <krystof@litomisky.com>
2020-10-20 18:13:43 -0700

    WIP

--- kindle_notes_to_md.py
+++ kindle_notes_to_md.py
@@ -4,8 +4,11 @@ Convert an HTML file of book notes exported from an Amazon Kindle
 to a Markdown document
 '''
 import argparse
+from html.parser import HTMLParser
 import sys
 import traceback
+
+from bs4 import BeautifulSoup
 
 from eglogging import *
 logging_load_human_config()
@@ -22,6 +25,7 @@ class Kindle_notes:
     self.author     = ''
 
     # only those chapters for which notes exist
+    # each is a (title, [notes/highlights]) pair
     self.chapters = []
 
     # each chapter has notes & highlights
@@ -31,13 +35,54 @@ class Kindle_notes:
 
   def parse_file(self, html_file: str):
     # parse an input HTML file
-    ERROR("TODO")
+
+    # read the file to a string
+    with open(html_file, 'r') as fp:
+      htmls = fp.read()
+
+    # parse the string
+    soup = BeautifulSoup(htmls, 'html.parser')
+
+    # go through all the relevant parts
+    all_divs = soup.find_all('div')
+    for div in all_divs:
+      # the class of the div
+      c = div['class'][0]
+
+      # handle title and author
+      if c == 'bookTitle':
+        self.book_title = div.string.strip()
+      elif c == 'authors':
+        self.author = div.string.strip()
+
+      # handle notes & highlights
+      elif c == 'sectionHeading':
+        # add a new empty chapter
+        self.chapters.append((div.string.strip(), []))
+
 
 
 
   def write_to_file(self, outfile : str = 'output.md'):
     # write the markdown file
-    ERROR("TODO")
+
+    # title & author
+    md =  "- Meta:\n"
+    md += "  - title: {}\n".format(self.book_title)
+    md += "  - author: {}\n".format(self.author)
+    md += "  - tags: #Books\n"
+
+    # all the highlights
+    md += "- # Raw Highlights & Notes:\n"
+    for chapter in self.chapters:
+      md += "  - ## {}\n".format(chapter[0])
+
+    WARN("TODO: check if the file exists and handle as appropriate")
+
+    with open(outfile, 'w') as fp:
+      fp.write(md)
+
+    INFO("Wrote the output to {}".format(outfile), LOG_COLORS['GREEN'])
 
 
 
